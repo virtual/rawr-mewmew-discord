d1c27eb68e6c56dd8edcd3b1015d054a50378730
Jeanine Schoessler <jeanine.mt@gmail.com>
2021-09-18 15:03:13 -0600

    add reply

--- bot.py
+++ bot.py
@@ -13,4 +13,13 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hello'):
+        await message.channel.send('👾 Why hello, ' + str(message.author))
+
+
 client.run(TOKEN)
